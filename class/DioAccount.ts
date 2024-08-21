e506c673ed4c02e10fe0d46eccce3041921d7110
Matheus Gregorio <feijaogreg@gmail.com>
2024-08-21 01:23:20 -0300

    dio account deposit and withdraw

--- class/DioAccount.ts
+++ class/DioAccount.ts
@@ -3,6 +3,10 @@ export abstract class DioAccount {
   private readonly accountNumber: number
   balance: number = 0
   private status: boolean = true
+  // [ ] Implementar os métodos de depósito (deposit) e saque (withdraw) na classe DioAccount
+  // - Os valores dos saldos devem ser alterados, de acordo com o valor informado para depósito
+  // - Apenas contas com o status true e saldo (balance) maior que o valor solicitado podem fazer saques
+
 
   constructor(name: string, accountNumber: number){
     this.name = name
@@ -18,14 +22,29 @@ export abstract class DioAccount {
     return this.name
   }
 
-  deposit = (): void => {
+  deposit = (value: number): number | void => {
     if(this.validateStatus()){
+      this.balance += value
       console.log('Voce depositou')
+      return this.balance;
     }
+
+    throw Error("Conta não ativa.")
   }
 
-  withdraw = (): void => {
-    console.log('Voce sacou')
+  withdraw = (value: number): number | void => {
+    if(this.validateStatus()){
+      if (this.balance >= value) {
+        this.balance -= value
+        console.log('Voce sacou')
+        return this.balance;
+      } else {
+        throw Error("Saldo insuficiente.")
+      }
+    }
+
+    throw Error("Conta não ativa.")
+
   }
 
   getBalance = (): void => {
